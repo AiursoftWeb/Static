0fa696f7fb21c657a376d16b07cbda8b1d43ac9d
Anduin Xue <xuef@microsoft.com>
2023-08-17 06:08:25 +0000

    More tests.

--- tests/Aiursoft.Static.Tests/IntegrationTests.cs
+++ tests/Aiursoft.Static.Tests/IntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
 
 namespace Aiursoft.Static.Tests
 {
@@ -20,13 +21,36 @@ namespace Aiursoft.Static.Tests
             await _server.StartAsync();
         }
 
+        [TestCleanup]
+        public async Task TestCleanup()
+        {
+            await _server.StopAsync();
+        }
+
         [TestMethod]
-        public async Task CallServer()
+        public async Task CallServerRoot()
         {
             var http = new HttpClient();
             var result = await http.GetAsync("http://localhost:8080");
             var content = await result.Content.ReadAsStringAsync();
             Assert.IsTrue(content.Contains("Hello"));
+        }
+
+        [TestMethod]
+        public async Task CallServerIndex()
+        {
+            var http = new HttpClient();
+            var result = await http.GetAsync("http://localhost:8080/index.html");
+            var content = await result.Content.ReadAsStringAsync();
+            Assert.IsTrue(content.Contains("Hello"));
+        }
+
+        [TestMethod]
+        public async Task CallServer404()
+        {
+            var http = new HttpClient();
+            var result = await http.GetAsync("http://localhost:8080/404.html");
+            Assert.AreEqual((HttpStatusCode)404, result.StatusCode);
         }
     }
 }
