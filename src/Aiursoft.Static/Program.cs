b41d5ada636d8bc2ac4110be646a59b64570ed4f
Anduin Xue <xuef@microsoft.com>
2023-08-17 06:06:50 +0000

    Write UT.

--- src/Aiursoft.Static/Program.cs
+++ src/Aiursoft.Static/Program.cs
@@ -1,43 +1,62 @@
 ﻿using System.CommandLine;
 
-var fileOption = new Option<string>(
-    name: "--path",
-    getDefaultValue: () => ".",
-    description: "The folder to start the server.");
-var portOption = new Option<int>(
-    name: "--port",
-    getDefaultValue: () => 8080,
-    description: "The port to listen for the server.");
+namespace Aiursoft.Static;
 
-var rootCommand = new RootCommand("A simple static files HTTP server.");
-rootCommand.AddOption(fileOption);
-rootCommand.AddOption(portOption);
-rootCommand.SetHandler(async (path, port) =>
+public class Program
 {
-    var contentRoot = Path.GetFullPath(path);
-    var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+    public static async Task Main(string[] args)
     {
-        WebRootPath = contentRoot
-    });
-    builder.Logging.AddSimpleConsole(options =>
-    {
-        options.IncludeScopes = false;
-        options.SingleLine = true;
-        options.TimestampFormat = "mm:ss:fff ";
-        options.UseUtcTimestamp = true;
-    });
-    builder.Logging.SetMinimumLevel(LogLevel.Information);
-    builder.WebHost.ConfigureKestrel(options => options.ListenAnyIP(port));
-    var host = builder.Build();
-    host.UseDefaultFiles(new DefaultFilesOptions
-    {
-        DefaultFileNames = new List<string> { "index.html", "index.htm" }
-    });
-    host.UseStaticFiles(new StaticFileOptions
-    {
-        ServeUnknownFileTypes = true
-    });
-    await host.RunAsync();
-}, fileOption, portOption);
+        var rootCommand = BuildCommand();
+        await rootCommand.InvokeAsync(args);
+    }
 
-await rootCommand.InvokeAsync(args);
+    public static RootCommand BuildCommand()
+    {
+        var fileOption = new Option<string>(
+            name: "--path",
+            getDefaultValue: () => ".",
+            description: "The folder to start the server.");
+        var portOption = new Option<int>(
+            aliases: new string[] { "--port", "-p" },
+            getDefaultValue: () => 8080,
+            description: "The port to listen for the server.");
+
+        var rootCommand = new RootCommand("A simple static files HTTP server.");
+        rootCommand.AddOption(fileOption);
+        rootCommand.AddOption(portOption);
+        rootCommand.SetHandler(async (path, port) =>
+        {
+            var host = BuildApp(path, port);
+            await host.RunAsync();
+        }, fileOption, portOption);
+        return rootCommand;
+    }
+
+    public static WebApplication BuildApp(string path, int port)
+    {
+        var contentRoot = Path.GetFullPath(path);
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            WebRootPath = contentRoot
+        });
+        builder.Logging.AddSimpleConsole(options =>
+        {
+            options.IncludeScopes = false;
+            options.SingleLine = true;
+            options.TimestampFormat = "mm:ss:fff ";
+            options.UseUtcTimestamp = true;
+        });
+        builder.Logging.SetMinimumLevel(LogLevel.Information);
+        builder.WebHost.ConfigureKestrel(options => options.ListenAnyIP(port));
+        var host = builder.Build();
+        host.UseDefaultFiles(new DefaultFilesOptions
+        {
+            DefaultFileNames = new List<string> { "index.html", "index.htm" }
+        });
+        host.UseStaticFiles(new StaticFileOptions
+        {
+            ServeUnknownFileTypes = true
+        });
+        return host;
+    }
+}
---
+++ tests/Aiursoft.Static.Tests/IntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aiursoft.Static.Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        public string _testPath;
+        public WebApplication _server;
+        public IntegrationTests()
+        {
+            _testPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets");
+            _server = Program.BuildApp(_testPath, 8080);
+        }
+
+        [TestInitialize]
+        public async Task TestInit()
+        {
+            await _server.StartAsync();
+        }
+
+        [TestMethod]
+        public async Task CallServer()
+        {
+            var http = new HttpClient();
+            var result = await http.GetAsync("http://localhost:8080");
+            var content = await result.Content.ReadAsStringAsync();
+            Assert.IsTrue(content.Contains("Hello"));
+        }
+    }
+}
