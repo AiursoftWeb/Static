24a5370f371dcff30a2986e55d66efc9d5c31195
xuef <xuef@microsoft.com>
2024-04-23 06:36:26 +0000

    Refactor variable name in NotFoundMiddleware
    Removed the underscore from the "next" variable in the NotFoundMiddleware class. The refactored code is now more in line with naming conventions and increases readability. This includes all instances where the variable "next" is utilized within the class.

--- src/Aiursoft.Static/Middlewares/NotFoundMiddleware.cs
+++ src/Aiursoft.Static/Middlewares/NotFoundMiddleware.cs
@@ -3,18 +3,18 @@ using Microsoft.Extensions.Options;
 
 namespace Aiursoft.Static.Middlewares;
 
-public class NotFoundMiddleware(RequestDelegate _next, IOptions<NotFoundConfiguration> options)
+public class NotFoundMiddleware(RequestDelegate next, IOptions<NotFoundConfiguration> options)
 {
     private readonly NotFoundConfiguration _options = options.Value; 
     
     public async Task InvokeAsync(HttpContext context)
     {
-        await _next(context);
+        await next(context);
         if (context.Response.StatusCode == 404)
         {
             var origPath = context.Request.Path;
             context.Request.Path = _options.NotFoundPage!;
-            await _next(context);
+            await next(context);
             context.Request.Path = origPath; // For correct logging and middleware compatibility
         }
     }
